ca60ca9374c4a25e976baf4cb9ee12c310fe3f2f
ronh <ronnahom96@gmail.com>
2016-07-20 19:04:59 +0300

    first commit all work no errors login with facebook work

--- app/src/main/java/com/example/ronhfreeman/getbabysitter/activities/MainActivity.java
+++ app/src/main/java/com/example/ronhfreeman/getbabysitter/activities/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.ronhfreeman.getbabysitter.activities;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -20,15 +18,10 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
         // Set the facebook
         FacebookSdk.sdkInitialize(getApplicationContext());
 
-        // Create the fragment manaager
-        FragmentManager fragmentManager = getFragmentManager();
-
-        // Begin the transaction
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
         // Create and add the new fragment
         LoginFragment lgnFragment = new LoginFragment();
-        fragmentTransaction.add(R.id.main, lgnFragment, lgnFragment.getClass().getName()).commit();
+        getFragmentManager().beginTransaction().
+                add(R.id.main, lgnFragment, lgnFragment.getClass().getName()).commit();
     }
 
     @Override
--- app/src/main/java/com/example/ronhfreeman/getbabysitter/fragments/LoginFragment.java
+++ app/src/main/java/com/example/ronhfreeman/getbabysitter/fragments/LoginFragment.java
@@ -14,8 +14,13 @@ import com.example.ronhfreeman.getbabysitter.R;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,46 +35,19 @@ import butterknife.ButterKnife;
  * create an instance of this fragment.
  */
 public class LoginFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private OnFragmentInteractionListener mListener;
 
     public LoginFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
+    public static LoginFragment newInstance() {
         LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     // Data members
@@ -103,13 +81,19 @@ public class LoginFragment extends Fragment {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        txtInfo.setText(
-                                "User ID: "
-                                        + loginResult.getAccessToken().getUserId()
-                                        + "\n" +
-                                        "Auth Token: "
-                                        + loginResult.getAccessToken().getToken()
-                        );
+                        txtInfo.setText("User ID: " + loginResult.getAccessToken().getUserId() + "\n" +
+                                        "Auth Token: " + loginResult.getAccessToken().getToken());
+
+                        // Get the details from the user
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(),
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject object, GraphResponse response) {
+                                        txtInfo.setText(object.toString());
+                                    }
+                                });
+                        request.executeAsync();
                     }
 
                     @Override
@@ -120,14 +104,12 @@ public class LoginFragment extends Fragment {
                     @Override
                     public void onError(FacebookException exception) {
                         txtInfo.setText("Login attempt failed.");
-
                     }
                 });
 
        return (view);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -151,18 +133,7 @@ public class LoginFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 
